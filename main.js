1f2b1ba06e93ff94d1608638e6071d21e16e9118
Jake Wood <jakealistairwood@gmail.com>
2020-10-02 09:50:20 +0100

    base styles in place for morse generator

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
+// You’re tasked with creating a website that can translate between english and morse code.
+// As an extension it should be able to translate both ways.
+
 // Store the Alphabet
+const alphabetArray = ["A", "B", "C", "D", "E", "F", "G",
+                       "H", "I", "J", "K", "L", "M", "N", 
+                       "O", "P", "Q", "R", "S", "T", "U",
+                       "V", "W", "X", "Y", "Z"];
 // Store the morse code
+const alphabetArray = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
+                       "....", "..", ".---", "-.-", ".-..", "--", "-.", 
+                       "---", ".--.", "--.-", ".-.", "...", "-", "..-",
+                       "...-", ".--", "-..-", "-.--", "--.."];
 // Listener on the button for click event
+const generateMorse = document.querySelector('.generate');
+
+generateMorse.addEventListener('click', () => {
+    
+})
 // ON CLICK -> Run a function -> Convert english to morse 
+
 // 'Convert english to morse' -> get the Letter, and find the corresponding morse 
 
 // Create two arrays -> one for letters of alphabet, one with the morse
