99ad0e67b058b7dc15a9417a2b0fd141772b5861
Jake Wood <jakealistairwood@gmail.com>
2020-10-02 09:33:11 +0100

    boilerplate in place for morse-code challenge

---
+++ main.js
@@ -0,0 +1,7 @@
+// Store the Alphabet
+// Store the morse code
+// Listener on the button for click event
+// ON CLICK -> Run a function -> Convert english to morse 
+// 'Convert english to morse' -> get the Letter, and find the corresponding morse 
+
+// Create two arrays -> one for letters of alphabet, one with the morse
